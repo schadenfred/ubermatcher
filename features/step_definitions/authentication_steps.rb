2ec4512cc891b308a3589044e2119a47aeb6b9eb
Frederick Schoeneman <fred.schoeneman@gmail.com>
2012-05-28 13:35:22 -0700

    installed simple form

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,39 @@
+Given /^no user exists with an email of "(.*?)"$/ do |email|
+  empty = User.find_by_email(email)
+  unless empty.nil?
+    empty.delete
+  end
+end
+
+Given /^I am not logged in$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I am on the home page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I follow the "(.*?)" link$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)" in the title$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I fill in the following:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I press "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see confirmation instructions in the subject$/ do
+  pending # express the regexp above with the code you wish you had
+end
